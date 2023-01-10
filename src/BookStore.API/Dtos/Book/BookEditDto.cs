6cd960b2672131fc93bd1c88f3e6703721475893
Angel Pineda <angel9_51@hotmail.com>
2023-01-10 08:58:06 -0600

    Controllers

---
+++ src/BookStore.API/Dtos/Book/BookEditDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.API.Dtos.Book
+{
+    public class BookEditDto
+    {
+        [Key]
+        public int Id { get; set; } 
+
+        [Required(ErrorMessage = "This field {0} is required")]
+        public int categoryId { get; set; }
+
+        [Required(ErrorMessage = "This field {0} is required")]
+        [StringLength(150, ErrorMessage = "This field {0} must be between {2} and {1} characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "This field {0} is required")]
+        [StringLength(150, ErrorMessage = "This field {0} must be between {2} and {1} characters")]
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+        public double Value { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
--- src/BookStore.API/Program.cs
+++ src/BookStore.API/Program.cs
@@ -2,28 +2,48 @@
 using BookStore.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using AutoMapper;
+using BookStore.API.Configuration;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-// Add services to the container.
-//builder.Services.AddControllers();
-builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAutoMapper(typeof(AutomapperConfig));
+// Add services to the container.
+builder.Services.AddControllers();
+
+//builder.Services.AddControllersWithViews();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 //builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Version = "v1",
+        Title = "BookStore API"
+    });
+});
 
+builder.Services.ResolveDependencies();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+/*
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}*/
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
